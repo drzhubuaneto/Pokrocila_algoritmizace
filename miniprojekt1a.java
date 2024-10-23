5bf08bb7ded49dec66ce13beb4038bd3e89e67c4
zelinane <zelinane@cvut.cz>
2024-10-23 11:03:16 +0200

    miniprojektjupi

---
+++ miniprojekt1a.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class miniprojekt1a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("vlozte pocet oken ktere umyl brigadnik 1: ");
+        int okna1 = sc.nextInt();
+        if (okna1 < 10) {
+            System.out.println("malo vykonny brigadnik = vyradit!");
+        }
+        System.out.println("vlozte pocet oken ktere umyl brigadnik 2: ");
+        int okna2 = sc.nextInt();
+        if (okna2 < 10) {
+            System.out.println("malo vykonny brigadnik = vyradit!");
+        }        
+        System.out.println("vlozte pocet oken ktere umyl brigadnik 3: ");
+        int okna3 = sc.nextInt();
+        if (okna3 < 10) {
+            System.out.println("malo vykonny brigadnik = vyradit!");
+        }
+        int oknacelkem = okna1 + okna2 + okna3;
+        System.out.printf("dnes bylo umyto %d oken a ", oknacelkem);
+        sc.close();
+
+        int maxokna;
+        if (okna1 > okna2 && okna1 > okna3) {
+            maxokna = okna1;
+        } else if (okna2 > okna3) {
+            maxokna = okna2;
+        } else {
+            maxokna = okna3;
+        }
+        System.out.printf("jeden brigadnik umyl max %d oken", maxokna);
+    }
+}
---
+++ miniprojekt1b.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class miniprojekt1b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("vlozte pocet brigadniku: ");
+        int pocetbrigadniku = sc.nextInt();
+
+        int celkemoken = 0;
+        int maxoken = 0;
+
+        for (int i=1; i <=pocetbrigadniku; i++) {
+            System.out.println("vlozte pocet oken kyere umyl birgadnik " + i + " ");
+            int okna = sc.nextInt();
+            celkemoken += okna;
+            if (okna > maxoken) {
+                maxoken = okna;
+            }
+            if (okna < 10) {
+                System.out.println("malo vykonny brigadnik = vyradit!");
+            }
+        }
+        sc.close();
+        
+        System.out.printf("dnes bylo umyto %d oken ", celkemoken);
+        System.out.printf("a jeden brigadnik umyl max %d oken ", maxoken);
+    }
+}
