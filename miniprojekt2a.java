bd61125d7499409dc521a395f67c2d65ce268d59
zelinane <zelinane@cvut.cz>
2024-10-30 10:27:51 +0100

    ukolik2

---
+++ miniprojekt2a.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class miniprojekt2a {
+    public static void main(String[] args) {
+        ArrayList<Integer> cisla = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int cislo;
+        int nasobekpo0;
+
+        System.out.println("zadej hodnoty cisel (zadej 0 pro ukonceni):");
+        while (true) {
+            System.out.print("zadej hodnotu cisla: ");
+            cislo = sc.nextInt();
+            if (cislo == 0) {
+                System.out.print("zadej nasobek: ");
+                nasobekpo0 = sc.nextInt();
+                break;
+            }
+            if (cislo >= 1) {
+                cisla.add(cislo);
+            } else {
+                System.out.println("neplatna hodnota");
+            }
+        }
+
+        System.out.println("\nzadane cisla:");
+        for (int i = 0; i < cisla.size(); i++) {
+            int hodnota = cisla.get(i);
+            System.out.println(hodnota);
+            }
+
+        System.out.println("\nnasobky cisel:");
+        for (int i = 0; i < cisla.size(); i++) {
+            int nasobek = cisla.get(i);
+            System.out.println(nasobek*nasobekpo0);
+            }
+        
+        sc.close();
+        }
+    
+}
